99d5baea066a8e0a187911823b25e8f5904c881b
Damian Centurion <>
2022-12-19 22:41:52 -0300

    Reflection utils

--- Assets/Scripts/Reflection/ReflectionUtils.cs
+++ Assets/Scripts/Reflection/ReflectionUtils.cs
@@ -1,22 +1,70 @@
 ﻿using System.Collections.Generic;
 using System;
 using System.Reflection;
+using DataStructures;
 
 namespace Reflection
 {
     public static class ReflectionUtils
     {
+        private static LookUpTable<Type, List<Type>> listOfImplementationsTable;
+        private static LookUpTable<string, Type> typesTable;
+        
         /// <summary>
         /// Returns all the Types that implements a given Class or Interface.
         /// </summary>
         public static List<Type> GetImplementationsOf<T>()
+        {
+            if (listOfImplementationsTable == null)
+            {
+                listOfImplementationsTable = new LookUpTable<Type, List<Type>>(GetImplementationsOfCreatorMethod);
+            }
+            
+            return listOfImplementationsTable.GetValue(typeof(T));
+        }
+        
+        public static Type GetType(string className)
+        {
+            if (typesTable == null)
+            {
+                typesTable = new LookUpTable<string, Type>(GetTypeCreatorMethod);
+            }
+            
+            return typesTable.GetValue(className);
+        }
+        
+        [UnityEditor.Callbacks.DidReloadScripts]
+        private static void OnScriptsReloaded() 
+        {
+            //Force recalculation.
+            listOfImplementationsTable = null; 
+            typesTable = null; 
+        }
+
+        private static Type GetTypeCreatorMethod(string className)
+        {
+            Type toReturn = null;
+            foreach (Assembly assembly in AppDomain.CurrentDomain.GetAssemblies())
+            {
+                foreach (Type type in assembly.GetTypes())
+                {
+                    if (type.Name == className)
+                    {
+                        toReturn = type;
+                    }
+                }
+            }
+            return toReturn;
+        }
+        
+        private static List<Type> GetImplementationsOfCreatorMethod(Type paramType)
         {
             List<Type> toReturn = new List<Type>();
             foreach (Assembly assembly in AppDomain.CurrentDomain.GetAssemblies())
             {
                 foreach (Type type in assembly.GetTypes())
                 {
-                    if (typeof(T).IsAssignableFrom(type))
+                    if (paramType.IsAssignableFrom(type))
                     {
                         toReturn.Add(type);
                     }
